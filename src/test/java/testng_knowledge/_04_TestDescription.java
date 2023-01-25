6393959863c60f76a512e0b48c5b049ab2454483
Abdallah Jaber <abdallahj708@gmail.com>
2023-01-24 20:47:58 -0600

    updated repo

--- src/test/java/testng_knowledge/_04_TestDescription.java
+++ src/test/java/testng_knowledge/_04_TestDescription.java
@@ -1,10 +1,9 @@
 package testng_knowledge;
 
-import org.testng.annotations.Ignore;
 import org.testng.annotations.Test;
 
 public class _04_TestDescription {
-
+/*
     @Test(priority = 1, description = "Login with valid credentials")
     public void login(){
         System.out.println("This is login");
@@ -15,4 +14,5 @@ public class _04_TestDescription {
     public void invalidLogin(){
         System.out.println("This is invalid login");
     }
+ */
 }
